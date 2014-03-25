fcc501d4297964cbb319c2ae4ca5ba360c0496cd
metalmike <mbodenbach777@gmail.com>
2014-03-25 21:43:47 +0100

    chunkparser iterate over track items

--- reascoremusicxml/reascoremusicxml.py
+++ reascoremusicxml/reascoremusicxml.py
@@ -22,8 +22,9 @@ def msg(m) :
     s = str(m) + '\n'
     RPR_ShowConsoleMsg(s)
 
-'Returns a list of the selected Tracks'
+
 def getSelectedTracksIdList():
+    'Returns a list of the selected Tracks'
     trackIdL = []
     selTrackCount = RPR_CountSelectedTracks(0)
 
@@ -36,16 +37,49 @@ def getSelectedTracksIdList():
         return trackIdL
 
 
+def getTrackItemsIdList(trackId):
+    'Returns a list of the items in a track'
+    itemIdL = []
+    trackItemCount = RPR_CountTrackMediaItems(trackId)
+    msg(trackItemCount)
+ 
+    if trackItemCount == 0:
+        msg("No item(s) in track\n")
+        return 0
+    for i in range(trackItemCount):
+        itemId = RPR_GetTrackMediaItem(trackId, i)
+        itemIdL.append(itemId)
+    return itemIdL
+
+
 def GetTrackName(trackId):
     currentName = str(RPR_GetSetMediaTrackInfo_String(trackId, "P_NAME", "", 0)[3])
     msg(currentName)
     return currentName
 
-    
+
+def chunk_parser(chunkLists):
+    midiEventListRaw = []
+    ticksPerQuarterNote = None
+
+    for i, rpr_chunk in enumerate(chunkLists):
+        for j, rpr_chunk_part in enumerate(rpr_chunk):
+            if rpr_chunk_part.startswith("HASDATA "):
+                if (ticksPerQuarterNote == None):
+                    ticksPerQuarterNote = rpr_chunk_part.split(" ")[2]
+                elif (ticksPerQuarterNote == rpr_chunk_part.split(" ")[2]):
+                    pass
+                else:
+                    raise("Different ticksPerQuarterNote in chunks! - Not supported!")
+                
+            if rpr_chunk_part.startswith("E ")  or rpr_chunk_part.startswith("e "):
+                midiEventListRaw.append(rpr_chunk_part.split(" ")[1:])
+    return ticksPerQuarterNote, midiEventListRaw
+        
+        
 def Generate():
     rpr_chunk = ""
     rpr_chunkLists = []
-    rpr_track_ItemIdL = []    
     
     s1 = stream.Score()
     s1.metadata = metadata.Metadata()
@@ -60,28 +94,19 @@ def Generate():
 
     rpr_trackList = getSelectedTracksIdList()
     GetTrackName(rpr_trackList[0])
-    rpr_itemsInTrack = RPR_CountTrackMediaItems(rpr_trackList[0])
-    msg(rpr_itemsInTrack)
     
-    rpr_track_ItemId = RPR_GetTrackMediaItem(rpr_trackList[0], 0)
-    rpr_track_ItemIdL.append(rpr_track_ItemId)
-    
-    rpr_chunk = RPR_GetSetItemState2(rpr_track_ItemId, "", 1024*1024*4, 1)[2]
-    rpr_chunkLists.append(list(rpr_chunk.split("\n")))
-    #msg(rpr_chunk)
-
-    #midiStream = stream.Stream()
+    rpr_track_ItemIdL = getTrackItemsIdList(rpr_trackList[0])
+    for rpr_track_ItemId in rpr_track_ItemIdL:
+        rpr_chunk = RPR_GetSetItemState2(rpr_track_ItemId, "", 1024*1024*4, 1)[2]
+        rpr_chunkLists.append(list(rpr_chunk.split("\n")))
+ 
+    #midiStream = stream.Part()
     #midi.translate.midiStringToStream("", midiStream)
     
-    for rpr_chunk_part in rpr_chunk.split("\n"):
-        if rpr_chunk_part.startswith("HASDATA "):
-            ticksPerQuarterNote = rpr_chunk_part.split(" ")[2]
-            msg(ticksPerQuarterNote)
-            
-        if rpr_chunk_part.startswith("E ")  or rpr_chunk_part.startswith("e "):
-            #midiStream.append(others)
-            msg(rpr_chunk_part)
-    
+    ticksPerQuarterNote, midiEventListRaw = chunk_parser(rpr_chunkLists)    
+
+    msg(ticksPerQuarterNote)
+    msg(midiEventListRaw)
     
     
     n1 = note.Note('g3', type='half')
